70ec01b9e564fb7678355e6650f501c105998fd8
prathyushnallamothu <64707528+prathyushnallamothu@users.noreply.github.com>
2020-06-01 16:41:18 +0530

    Update dbconnection.go

--- dbconnection.go
+++ dbconnection.go
@@ -9,7 +9,7 @@ import (
 )
 
 func Connect() *sql.DB {
-	db, err := sql.Open("mysql", "bxedpdnv1mniuhiszlm5-mysql.services.clever-cloud.com:BNNhc81iQJixAyW0aCZ6@tcp(bxedpdnv1mniuhiszlm5-mysql.services.clever-cloud.com:3306)/bxedpdnv1mniuhiszlm5")
+	db, err := sql.Open("mysql", "uswsnstc7pistx32:BNNhc81iQJixAyW0aCZ6@tcp(bxedpdnv1mniuhiszlm5-mysql.services.clever-cloud.com:3306)/bxedpdnv1mniuhiszlm5")
 	if err != nil {
 		log.Fatal(err)
 	}
